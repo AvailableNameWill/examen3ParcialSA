fa48f3c832adbb02c574b6861ac410226cd1c5d4
AvailableNameWill <carloswill06@gmail.com>
2023-12-16 15:54:48 -0600

    first

--- Ejercicio#2/exer.py
+++ Ejercicio#2/exer.py
@@ -2,10 +2,12 @@ arrEntrada = []
 global tokenspos
 global salida
 global cant
+global unknownChars
 #global texto
 salida = ""
 cant = 0
 tokenspos = {}
+unknownChars = []
 
 #texto = input('Ingrese la cadena')
 
@@ -23,6 +25,7 @@ with open(path, 'r') as file:
 # Función para encriptar la cadena
 def encriptar(texto):
     global tokenspos
+    global unknownChars
     resultado = []
     unknownChars = []
     for i, caracter in enumerate(texto):
@@ -40,11 +43,26 @@ def encriptar(texto):
     return resultado
 
 
+def desencript(encripted, unknown):
+    decript = []
+
+    for i, enchar in enumerate(encripted):
+        if enchar == 'N' and unknown:
+            decript.append(unknown.pop(0))
+        else:
+            decript.append(dict.get(enchar, enchar))
+    return ''.join(decript)
+
+
+
 texto = input('Ingrese la cadena')
 salida = encriptar(texto)
+original = desencript(salida, unknownChars)
 
 print('salida:', salida)
 print('longitud:', len(salida))
+print(unknownChars)
+print(original)
 
 
 
